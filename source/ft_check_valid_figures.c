e947b240d31f14c32d4a13dc5ed66c9340c777ee
Otho Jessi <ojessi@mi-p3.21-school.ru>
2019-04-21 19:05:24 +0300

    Full valid check

--- source/ft_check_valid_figures.c
+++ source/ft_check_valid_figures.c
@@ -6,7 +6,7 @@
 /*   By: ojessi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/21 15:09:53 by ojessi            #+#    #+#             */
-/*   Updated: 2019/04/21 18:27:01 by ojessi           ###   ########.fr       */
+/*   Updated: 2019/04/21 19:04:34 by ojessi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ static	char	**ft_create_place(void)
 	return (place);
 }
 
-static	void	ft_free_place(char ***place)
+static	void	ft_free_place(char ***place, char **strcp)
 {
 	char		**str;
 	int			i;
@@ -85,6 +85,8 @@ static	void	ft_free_place(char ***place)
 	while (++i < 4)
 		free(str[i]);
 	free(str);
+	if (ft_strlen(*strcp) == 1)
+		ft_print_error();
 }
 
 int				ft_check_valid_figures(int fd)
@@ -110,6 +112,6 @@ int				ft_check_valid_figures(int fd)
 			figures += ft_check_side(place);
 		}
 	}
-	ft_free_place(&place);
+	ft_free_place(&place, &str);
 	return (figures);
 }
